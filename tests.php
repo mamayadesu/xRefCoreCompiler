c4318496df98765513361a4319fe3cb2a25ea384
Semyon "xRef <mamayadesu@gmail.com>
2022-02-16 22:45:11 +0500

    Update tests.php

--- tests.php
+++ tests.php
@@ -3,21 +3,21 @@
 echo "Checking your PHP version... ";
 if (version_compare(phpversion(), "7.4", "<"))
 {
-    echo "FAIL. xRefCoreCompiler requires PHP 7.4 at least. Your version is " . phpversion();
+    echo "FAIL. xRefCoreCompiler requires PHP 7.4 at least. Your version is " . phpversion() . ".\n";
     exit(255);
 } else echo "OK\n";
 
 echo "Checking Sockets extension... ";
 if (!extension_loaded("sockets"))
 {
-    echo "FAIL. Sockets extension is not loaded or installed";
+    echo "FAIL. Sockets extension is not loaded or installed.\n";
     exit(255);
 } else echo "OK\n";
 
 echo "Checking PHAR... ";
 if (ini_get("phar.readonly") == "1")
 {
-    echo "FAIL. PHAR has read-only mode. Open your 'php.ini' and set phar.readonly to Off";
+    echo "FAIL. PHAR has read-only mode. Open your 'php.ini' and set phar.readonly to Off.\n";
     exit(255);
 } else echo "OK\n";
 exit(0);
