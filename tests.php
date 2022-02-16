83c33d7c79408376e2c8973c5722e17f6745303f
Semyon "xRef <mamayadesu@gmail.com>
2022-02-16 20:20:42 +0500

    Add files via upload

---
+++ tests.php
@@ -0,0 +1,23 @@
+<?php
+
+echo "Checking your PHP version... ";
+if (version_compare(phpversion(), "7.4", "<"))
+{
+    echo "FAIL. xRefCoreCompiler requires PHP 7.4 at least. Your version is " . phpversion();
+    exit(255);
+} else echo "OK\n";
+
+echo "Checking Sockets extension... ";
+if (!extension_loaded("sockets"))
+{
+    echo "FAIL. Sockets extension is not loaded or installed";
+    exit(255);
+} else echo "OK\n";
+
+echo "Checking PHAR... ";
+if (ini_get("phar.readonly") == "1")
+{
+    echo "FAIL. PHAR has read-only mode. Open your 'php.ini' and set phar.readonly to Off";
+    exit(255);
+} else echo "OK\n";
+exit(0);
